608d3f44f8bfa686245801fb44bcb9638d17f34d
binhussen <mhdhsna@gmail.com>
2022-07-18 21:47:51 +0300

    Add mobile-menu functionality by DOM

---
+++ script.js
@@ -0,0 +1,30 @@
+/** @format */
+
+const logo = document.querySelector('.logo');
+const navMenu = document.querySelector('.nav-menu');
+const hamburger = document.querySelector('.hamburger');
+const navItem = document.querySelectorAll('.nav-item');
+
+const openModal = () => {
+  logo.classList.toggle('active');
+  navMenu.classList.remove('desktop');
+  navMenu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+};
+
+const closeModal = () => {
+  navMenu.classList.add('desktop');
+  logo.classList.remove('active');
+  navMenu.classList.remove('active');
+  hamburger.classList.remove('active');
+};
+
+hamburger.addEventListener('click', () => {
+  openModal();
+});
+
+navItem.forEach((n) => {
+  n.addEventListener('click', () => {
+    closeModal();
+  });
+});
